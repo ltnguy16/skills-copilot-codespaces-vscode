95f49f55db73a2cc15a5f6ed334b2b3ff4e86e35
ltnguy16 <ltnguyen1995@gmail.com>
2023-12-03 16:52:02 +0000

    Add member

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillMember() {
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberArray = [memberValue, memberText];
+    return memberArray;
+}
